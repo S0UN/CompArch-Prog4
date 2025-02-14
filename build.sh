bdd72f4face81edd4cc1a663fa9571c0108b31ce
Chotepong Victor Tangton <65469344+S0UN@users.noreply.github.com>
2025-02-14 10:51:16 -0600

    w

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Compile the unified executable (syntax_verifier.c now includes binary conversion)
-gcc -o hw4 -Iinclude src/arraylist.c src/label_table.c src/line.c src/syntax_verifier.c
+gcc -o hw5 -Iinclude src/arraylist.c src/label_table.c src/line.c src/syntax_verifier.c
 
 # Move the compiled executable to the script's directory (if it's not already there)
-mv hw4 "$(dirname "$0")"
+mv hw5 "$(dirname "$0")"
 
 # Make the executable runnable
 chmod +x hw5
