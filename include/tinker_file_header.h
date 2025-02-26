9c33952c52418f1e848e6f6c9c413ee487d00b49
Chotepong Victor Tangton <65469344+S0UN@users.noreply.github.com>
2025-02-26 14:44:38 -0600

    w

--- include/tinker_file_header.h
+++ include/tinker_file_header.h
@@ -1,7 +1,8 @@
-typedef struct {
-    unsigned int file_type;      // Always 0 for now.
-    unsigned int code_seg_begin; // For this project, code loads at 0x2000.
-    unsigned int code_seg_size;  // In bytes.
-    unsigned int data_seg_begin; // For this project, data loads at 0x10000.
-    unsigned int data_seg_size;  // In bytes.
+typedef struct
+{
+    uint64_t file_type;      // Always 0 for now.
+    uint64_t code_seg_begin; // For this project, code loads at 0x2000.
+    uint64_t code_seg_size;  // In bytes.
+    uint64_t data_seg_begin; // For this project, data loads at 0x10000.
+    uint64_t data_seg_size;  // In bytes.
 } TinkerFileHeader;
